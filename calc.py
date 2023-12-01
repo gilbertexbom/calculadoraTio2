cb578671f7211dc0b344a664952ddd112ebadd75
CrisYuri <cristina.yuri@gmail.com>
2023-12-01 14:22:35 -0300

    - Commit da Cris

--- calc.py
+++ calc.py
@@ -6,9 +6,11 @@ def sub(v1, v2):
     return v1 - v2
 def mult(v1, v2):
     return v1 * v2
+def exp(v1, v2):
+    return v1 ** v2
 
 # teste
 # print(f'{2} + {2} = {soma(2, 2)}')
 # print(f'{2} - {2} = {sub(2, 2)}')
 # print(f'{2} * {3} = {mult(2, 3)}')
-
+# print(f'{2} ** {3} = {exp(2, 3)}')
