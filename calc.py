43ba3f7ff7b10a4c1ecfc385fe38903576b8119e
Aluno <valtertert@>
2023-12-01 14:06:49 -0300

    Commit do léo

--- calc.py
+++ calc.py
@@ -2,7 +2,10 @@
 
 def soma(v1, v2):
     return v1 + v2
+def sub(v1, v2):
+    return v1 - v2
 
 # teste
-# print(f'{2} + {2} = {soma(2, 2)}')
+#print(f'{2} + {2} = {soma(2, 2)}')
+#print(f'{2} - {2} = {sub(2, 2)}')
 
--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,6 @@
 # Interface da calculadora
-import calc
+from calc import soma
+from calc import sub
 
 # Apresentação
 print('\n\t\t\t -- Calculadora Simples --\n')
@@ -9,7 +10,9 @@ n1 = int(input('Informe n1: '))
 n2 = int(input('Informe n2: '))
 
 # Processamento
-total = calc.soma(2, 2)
+total = soma(n1, n2)
+total2 = sub(n1, n2)
 
 # Saída
 print(f'{n1} + {n2} = {total}')
+print(f'{n1} - {n2} = {total2}')
