cdc0b47811cfe029e3ee814f66dcf676330e907a
Evaldo-Gomes <evaldo_gomes@hotmail.com>
2023-12-01 14:33:50 -0300

    Incluído operação de radiciação

--- calc.py
+++ calc.py
@@ -9,8 +9,12 @@ def mult(v1, v2):
 def exp(v1, v2):
     return v1 ** v2
 
+def rad(v1):
+    return v1 ** (1/2)
+
 # teste
 # print(f'{2} + {2} = {soma(2, 2)}')
 # print(f'{2} - {2} = {sub(2, 2)}')
 # print(f'{2} * {3} = {mult(2, 3)}')
 # print(f'{2} ** {3} = {exp(2, 3)}')
+print(f'A raíz quadrada de {2} é: {rad(2)}')
