e3e3a70a35fb77886e5b1ac4d3a2618e8ddca5f5
Viny <viniciusfernandes1357@gmail.com>
2023-12-01 14:17:15 -0300

    - commit do Alysson

--- calc.py
+++ calc.py
@@ -4,8 +4,11 @@ def soma(v1, v2):
     return v1 + v2
 def sub(v1, v2):
     return v1 - v2
+def mult(v1, v2):
+    return v1 * v2
 
 # teste
-#print(f'{2} + {2} = {soma(2, 2)}')
-#print(f'{2} - {2} = {sub(2, 2)}')
+# print(f'{2} + {2} = {soma(2, 2)}')
+# print(f'{2} - {2} = {sub(2, 2)}')
+# print(f'{2} * {3} = {mult(2, 3)}')
 
