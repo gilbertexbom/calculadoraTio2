b2aa2d7547e83bb861ce416a33df31c83cee12a2
CrisYuri <cristina.yuri@gmail.com>
2023-12-06 14:10:17 -0300

    commit com def de divisão

--- calc.py
+++ calc.py
@@ -6,6 +6,8 @@ def sub(v1, v2):
     return v1 - v2
 def mult(v1, v2):
     return v1 * v2
+def div(v1, v2):
+    return v1 / v2
 def exp(v1, v2):
     return v1 ** v2
 
@@ -16,5 +18,6 @@ def rad(v1):
 # print(f'{2} + {2} = {soma(2, 2)}')
 # print(f'{2} - {2} = {sub(2, 2)}')
 # print(f'{2} * {3} = {mult(2, 3)}')
+# print(f'{6} * {3} = {div(6, 3)}')
 # print(f'{2} ** {3} = {exp(2, 3)}')
 # print(f'A raíz quadrada de {2} é: {rad(2)}')
