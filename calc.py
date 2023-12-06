e2ec328bb93bd756951c45c20f34c5b716e71691
CrisYuri <cristina.yuri@gmail.com>
2023-12-06 14:11:07 -0300

    commit com divisão com print correto

--- calc.py
+++ calc.py
@@ -18,6 +18,6 @@ def rad(v1):
 # print(f'{2} + {2} = {soma(2, 2)}')
 # print(f'{2} - {2} = {sub(2, 2)}')
 # print(f'{2} * {3} = {mult(2, 3)}')
-# print(f'{6} * {3} = {div(6, 3)}')
+# print(f'{6} / {3} = {div(6, 3)}')
 # print(f'{2} ** {3} = {exp(2, 3)}')
 # print(f'A raíz quadrada de {2} é: {rad(2)}')
